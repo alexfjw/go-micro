a88eacb175d94f834f33c0cb3be019b6b31eeeb1
Alex Fong <alexfongg@gmail.com>
2021-09-16 04:18:07 +0000

    done

--- event.go
+++ event.go
@@ -11,6 +11,7 @@ type event struct {
 	topic string
 }
 
+
 func (e *event) Publish(ctx context.Context, msg interface{}, opts ...client.PublishOption) error {
 	return e.c.Publish(ctx, e.c.NewMessage(e.topic, msg), opts...)
 }
